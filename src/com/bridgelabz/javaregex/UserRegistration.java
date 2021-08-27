6b72b7b23677a1967e38f2b5e28d42ce85b285d4
rajshetye11 <rajshetye11@gmail.com>
2021-08-27 17:44:36 +0530

    [raj] : added regex for username(name starts with cap and 3 char)

--- src/com/bridgelabz/javaregex/UserRegistration.java
+++ src/com/bridgelabz/javaregex/UserRegistration.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.javaregex;
 
+import java.util.Scanner;
+
 public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Valid UserName (must have minimum 3 characters) :");
+		String str = sc.nextLine();
+		String regex = "^?[A-Z][a-z]{2,25}";
+		
+		System.out.println(str.matches(regex));
+		
+		if(str.matches(regex)) {
+			System.out.println(str);
+		}else {
+			System.out.println("Please enter Valid Username");
+		}
 	}
 }
